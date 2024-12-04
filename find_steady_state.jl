1fec22b753384da48bff0d26c82c9e8cb08050f5
Henry S Hunt <henhen7.24@gmail.com>
2024-12-04 02:39:27 -0800

    julian method fixed

--- find_steady_state.jl
+++ find_steady_state.jl
@@ -91,42 +91,32 @@ end
 
 λ = find_lambda(; λ0=2.0, P=P, ω_tilde=ω_tilde, N_A=N_A, κ=κ, E_0=E_0, ω_r=ω_r, longmax=longmax, transmax=transmax, n=1, rate=0.1)
 
+vec_dim = 1 + (2 * longmax + 1) * (2 * transmax + 1)
+u0 = (1 - im) / (2 * sqrt(2 * N_A)) * ones(ComplexF64, vec_dim)
+u0[2+to_1d_index(0, 0, transmax, longmax)] = 1.0
+u0[2+to_1d_index(1, 0, transmax, longmax)] = 0.0
+u0[2+to_1d_index(0, 1, transmax, longmax)] = 0.0
+u0[2+to_1d_index(-1, 0, transmax, longmax)] = 0.0
+u0[2+to_1d_index(0, -1, transmax, longmax)] = 0.0
+u0norm = sum(abs.(u0[2:end]) .^ 2)
+u0[2:end] = u0[2:end] / sqrt(u0norm)
 
-function julians_method(; λ0=0.0, P=0.0, ω_tilde=0.0, N_A=10^5, κ=8.1, E_0=40.0, ω_r=0.05, longmax=10, transmax=10, n=1, dt=1.0)
-    λ = λ0
-    tol = 1e-5
-    max_iters = 10^5
-    iter = 0
-
-    vec_dim = (2 * longmax + 1) * (2 * transmax + 1)
-    u0 = (1 - im) / (2 * sqrt(2 * N_A)) * ones(ComplexF64, vec_dim)
-    u0[1+to_1d_index(0, 0, transmax, longmax)] = 1.0
-    u0[1+to_1d_index(1, 0, transmax, longmax)] = 0.0
-    u0[1+to_1d_index(0, 1, transmax, longmax)] = 0.0
-    u0[1+to_1d_index(-1, 0, transmax, longmax)] = 0.0
-    u0[1+to_1d_index(0, -1, transmax, longmax)] = 0.0
-    u0norm = sum(abs.(u0[2:end]) .^ 2)
-    u0 = u0 / sqrt(u0norm)
-
-    while iter < max_iters
-        iter += 1
-
-        cavity_value = cavity_eq_for_eigvec(n, λ, u0; P=P, ω_tilde=ω_tilde, N_A=N_A, κ=κ, E_0=E_0, ω_r=ω_r, longmax=longmax, transmax=longmax)
-
-
-        if abs(cavity_value) < tol
-            break
-        end
-
-        λ -= dt * cavity_value
-        println("New lambda $(λ) with resid $(norm(cavity_value))")
-    end
-
-    if iter == max_iters
-        println("Warning reached max iters.")
-    end
-
-    return λ
+function drift!(du, u, p, t)
+    u[2:end] = u[2:end] / norm(u[2:end])
+    dispative_dynamics!(du, u, p, t; P=P, ω_tilde=ω_tilde, N_A=N_A, κ=κ, E_0=E_0, ω_r=ω_r, longmax=longmax, transmax=transmax)
 end
 
-λ = julians_method(; λ0=2.0, P=P, ω_tilde=ω_tilde, N_A=N_A, κ=κ, E_0=E_0, ω_r=ω_r, longmax=longmax, transmax=transmax, n=1, dt=10^(-6))
+trecord = 0.0:0.05:500.0
+tspan = (trecord[begin], trecord[end])
+
+probODE = ODEProblem(drift!, u0, tspan)
+sol = solve(probODE, Tsit5(); reltol=10^-4, abstol=10^-4, dt=10^(-3), maxiters=10^12, save_noise=false, save_everystep=false, saveat=trecord)
+
+p = plot(sol.t, map(x -> real(x[1]), sol.u), xlabel="Time", ylabel="Cavity Field", label=L"Re[$\lambda$]")
+plot!(p, sol.t, map(x -> imag(x[1]), sol.u), xlabel="Time", ylabel="Cavity Field", label=L"Im[$\lambda$]")
+savefig(p, "JulianDynamics.png")
+
+duTest = 0.0 * similar(u0)
+multimomenta_model_drift!(duTest, sol.u[end], nothing, nothing; P=P, ω_tilde=ω_tilde, N_A=N_A, κ=κ, E_0=E_0, ω_r=ω_r, longmax=longmax, transmax=transmax)
+
+println("norm of duTest $(norm(duTest))")
