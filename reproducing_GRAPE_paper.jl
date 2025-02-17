76dee5076c4f8f03b34149eaba7ceebe49dc4975
Henry Hunt <henhen7.24@gmail.com>
2025-02-17 11:54:09 -0800

    reproducing feedback grape

---
+++ reproducing_GRAPE_paper.jl
@@ -0,0 +1,123 @@
+using QuantumOptics, ForwardDiff, Plots
+
+# Define the cutoff for the Fock space
+Nfock = 4
+
+# Create the Fock space basis
+b_fock = FockBasis(Nfock)
+b_spin = SpinBasis(1 // 2)
+
+# Create a composite Hilbert space
+bases = [b_fock, b_spin]
+full_basis = tensor(bases...)
+
+# Function to tensor an operator with identity operators in all other bases
+function mb(op, index, bases)
+    ops = [index == i ? op : identityoperator(bases[i]) for i in 1:length(bases)]
+    return tensor(ops...)
+end
+
+# Example usage of the mb function
+a_op = mb(destroy(b_fock), 1, [b_fock, b_spin])
+σ_minus = mb(sigmam(b_spin), 2, [b_fock, b_spin])
+
+# Print the tensor product operators
+println("Tensor product operator (a ⊗ I):")
+println(a_op)
+println("Tensor product operator (I ⊗ σ_minus):")
+println(σ_minus_op)
+
+# Function to create a displacement operator
+function displacement(alpha, a_op)
+    return exp(-im * (conj(alpha) * a_op + alpha * dagger(a_op)))
+end
+
+# Example usage of the displacement function
+alpha = 1.0 + 1.0im
+D_op = displacement(alpha, a_op)
+
+# Print the displacement operator
+println("Displacement operator D(α):")
+println(D_op)
+
+# Define a vector of 2-tuples of complex numbers
+alphas = [3.1, 3.1, 3.1]
+betas = [3.1, 3.1, 3.1]
+
+function op_evolution(op_0, alphas, betas)
+    op_t = [op_0]
+    for i in eachindex(alphas)
+        U_q = displacement(alphas[i], σ_minus / 2)
+        U_qc = displacement(betas[i], dagger(a_op) * σ_minus / 2)
+        new_op = U_q * U_qc * op_t[end] * dagger(U_qc) * dagger(U_q)
+        push!(op_t, new_op)
+    end
+    return op_t
+end
+
+function op_evolution_rev(op_0, alphas, betas)
+    op_t = [op_0]
+    for i in eachindex(alphas)
+        U_q = displacement(alphas[i], σ_minus / 2)
+        U_qc = displacement(betas[i], dagger(a_op) * σ_minus / 2)
+        new_op = dagger(U_qc) * dagger(U_q) * op_t[end] * U_q * U_qc
+        push!(op_t, new_op)
+    end
+    return reverse(op_t)
+end
+
+function calculate_grape_gradient(ρ_t, σ_t, alphas, betas)
+    alpha_grad = zeros(eltype(alphas), length(alphas))
+    beta_grad = zeros(eltype(betas), length(betas))
+    for i in eachindex(alphas)
+        U_qc = displacement(betas[i], dagger(a_op) * σ_minus / 2)
+
+        # Calculate the derivative of the unitary with respect to the real part of the displacement parameter
+        dU_qc_dbeta = (-im * (conj(betas[i]) * dagger(a_op) * σ_minus / 2 + betas[i] * a_op * dagger(σ_minus) / 2)) * displacement(betas[i], dagger(a_op) * σ_minus / 2)
+        dU_q_dalpha = (-im * (conj(alphas[i]) * σ_minus / 2 + alphas[i] * dagger(σ_minus) / 2)) * displacement(alphas[i], σ_minus / 2)
+
+        commutator = dU_qc_dbeta * ρ_t[i] - ρ_t[i] * dU_qc_dbeta
+        beta_grad[i] = tr(dagger(σ_t[i]) * commutator)
+        mid_ρ = U_qc * ρ_t[i] * dagger(U_qc)
+        commutator = dU_q_dalpha * mid_ρ - mid_ρ * dU_q_dalpha
+        mid_σ = U_qc * σ_t[i] * dagger(U_qc)
+        alpha_grad[i] = tr(dagger(mid_σ) * commutator)
+    end
+    return alpha_grad, beta_grad
+end
+
+# Example usage of the apply_control function
+initial_state = tensor(fockstate(b_fock, 0), spindown(b_spin))
+ρ_t = op_evolution(dm(initial_state), alphas, betas)
+target_state = tensor(fockstate(b_fock, 0), spinup(b_spin))
+σ_t = op_evolution_rev(dm(target_state), alphas, betas)
+
+alpha_grad, beta_grad = calculate_grape_gradient(ρ_t, σ_t, alphas, betas)
+
+ρ_t_boson = [ptrace(ρ, 2) for ρ in ρ_t]
+ρ_t_spin = [ptrace(ρ, 1) for ρ in ρ_t]
+
+function plot_wigner(ρ_t::Vector{<:Operator}; args...)
+    wigner_max_and_min = [
+        let W = wigner(ρ, range(-5, stop=5, length=100), range(-5, stop=5, length=100))
+            (maximum(W), minimum(W))
+        end for ρ in ρ_t
+    ]
+    wigner_max = maximum([max_t for (max_t, min_t) in wigner_max_and_min])
+    wigner_min = minimum([min_t for (max_t, min_t) in wigner_max_and_min])
+
+    anim = @animate for ρ in ρ_t
+        plot_wigner(ρ; clims=(wigner_min, wigner_max), args...)
+    end
+
+    gif(anim, "wigner_animation.gif", fps=10)
+end
+
+function plot_wigner(ρ::Operator; c=:viridis, xlabel="x", ylabel="p", title="Wigner Function", kwargs...)
+    x = y = range(-5, stop=5, length=100)
+    W = wigner(ρ, x, y)
+
+    heatmap(x, y, W; c=c, xlabel=xlabel, ylabel=ylabel, title=title, kwargs...)
+end
+
+plot_wigner(ρ_t_boson)
